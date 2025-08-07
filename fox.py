da672dd02d2b339cb9f7b2855709f1ac089bff82
nelianj <1032239033@pfur.ru>
2025-08-07 21:42:30 +0300

    added timestamp

--- fox.py
+++ fox.py
@@ -1,8 +1,10 @@
 import requests
+import time
+ts = time.time()
 random_fox = requests.get('https://randomfox.ca/floof')
 fox = random_fox.json()
 fox1 = fox['image']
 query_parameters = {"downloadformat": "jpg"}
 response = requests.get(fox1, params = query_parameters)
-with open('fox.jpg', 'wb') as file:
+with open('fox' +str(ts)+'.jpg', 'wb') as file:
     file.write(response.content)
